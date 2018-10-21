9921398a90b3ff61fd40476faa4b08c9d372db2d
eshna sengupta <eshna1995@hotmail.com>
2018-10-21 15:43:11 -0500

    changed name

--- DataExploration.R
+++ DataExploration.R
@@ -3,3 +3,5 @@ library(RCurl)
 #Reading in the data set directly from the github url
 ratings_url = getURL("https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/master/ratings.csv")
 ratings = read.csv(text = ratings_url)
+
+#data Exploration here
