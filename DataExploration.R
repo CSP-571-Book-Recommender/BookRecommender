ffda7a9350b1c289d85217a955386d08385392e8
eshna sengupta <eshna1995@hotmail.com>
2018-10-22 12:53:14 -0500

    basic exploration

--- DataExploration.R
+++ DataExploration.R
@@ -4,4 +4,29 @@ library(RCurl)
 ratings_url = getURL("https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/master/ratings.csv")
 ratings = read.csv(text = ratings_url)
 
-#start exploring the data
+#data Exploration here
+library(dplyr)
+library(ggplot2)
+head(ratings)
+
+#no. of ratings per user 
+rat <- ratings %>% 
+  group_by(user_id) %>%
+  summarise(ratings_per_user = n())
+qplot(rat$ratings_per_user, geom = "histogram", xlab = "Ratings per user", fill=I("grey"), 
+      col=I("black"))
+
+mean(rat$ratings_per_user)
+min(rat$ratings_per_user)
+max(rat$ratings_per_user)
+
+# no. of ratings per book
+rat <- ratings %>% 
+  group_by(book_id) %>%
+  summarise(ratings_per_book = n())
+qplot(rat$ratings_per_book, geom = "histogram", xlab = "Ratings per book", xlim = c(0, 1000), fill=I("grey"), 
+      col=I("black")) 
+
+mean(rat$ratings_per_book)
+max(rat$ratings_per_book)
+min(rat$ratings_per_book)
