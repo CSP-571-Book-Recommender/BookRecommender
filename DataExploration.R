7bc49d07bffa63bc5194206d379d29614759c435
VivekanandanSama <44374768+VivekanandanSama@users.noreply.github.com>
2018-10-24 14:58:50 -0500

    Genre and Language distributions added

--- DataExploration.R
+++ DataExploration.R
@@ -30,6 +30,7 @@ to_read <- read.csv(text = to_read_url)
 rm(to_read_url)
 
 #data Exploration here
+library(data.table)
 library(dplyr)
 library(reshape2)
 library(ggplot2)
@@ -97,3 +98,59 @@ top10_books <- merge(top10_books, books[,c("book_id", "original_title", "average
 active_readers <- ratings %>% group_by(user_id) %>% summarise(number_of_ratings = length(unique(book_id)))%>% 
                arrange(desc(number_of_ratings)) %>% mutate(rank = c(1:length(number_of_ratings))) %>% filter(rank <= 10) %>%
                select(user_id,number_of_ratings)
+
+###### genre frequency distribution
+main_tags_labels = c('romance','fiction','young-adult','fantasy','science-fiction',
+                     'children','best','covers','non-fiction', 'history','mystery',
+                     'paranormal','love','horror','historical','sci-fi',
+                     'historical-fiction','nonfiction','series','contemporary',
+                     'thriller','women','novels','suspense','classics' ,'graphic-novels', 
+                     'historical-romance', 'christian')
+
+genre_tags <- tags[tags$tag_name %in% main_tags_labels,]
+genre_tags$tag_name <- as.character(genre_tags$tag_name)
+
+book_genres <- as.data.frame(merge(book_tags,genre_tags, by = "tag_id", all = F))
+book_genres$tag_name <- as.character(book_genres$tag_name)
+
+book_genres[book_genres$tag_name == "nonfiction",c("tag_id", "tag_name")] <- genre_tags[genre_tags$tag_name == "non-fiction",]
+book_genres[book_genres$tag_name == "history",c("tag_id", "tag_name")] <- genre_tags[genre_tags$tag_name == "historical",]
+book_genres[book_genres$tag_name == "science-fiction",c("tag_id", "tag_name")] <- genre_tags[genre_tags$tag_name == "sci-fi",]
+book_genres[book_genres$tag_name == "love",c("tag_id", "tag_name")] <- genre_tags[genre_tags$tag_name == "romance",]
+
+book_genres <- as.data.frame(book_genres %>% group_by(goodreads_book_id,tag_id,tag_name) %>% summarise(count = sum(count)))
+
+book_genre_dist <- as.data.frame(table(book_genres$tag_name))
+names(book_genre_dist) <- c("genre", "Freq")
+book_genre_dist$genre <- as.character(book_genre_dist$genre)
+
+ggplot(book_genre_dist, aes(x=genre, y=Freq)) + 
+  geom_bar(stat="identity", width=.5, fill="tomato3") + 
+  labs(title="Frequency Distribution of Genre") + 
+  theme(axis.text.x = element_text(angle=0, vjust=0.6)) + 
+  geom_text(aes(label=Freq), hjust = -0.25, vjust= 0.5, color="black", size=3.5)+coord_flip()
+
+###### language frequency distribution
+lang <- as.data.frame(table(books$language_code))
+names(lang)[1] <- "Language"
+lang$Language <- as.character(lang$Language)
+lang <- lang[lang$Language != "",]
+lang <- lang[order(lang$Freq, decreasing = T),]
+lang$Language[!lang$Language %in% lang$Language[1:5]] <- "Others"
+lang <- as.data.frame(lang %>% group_by(Language) %>% summarise(count = sum(Freq)))
+
+ggplot(lang, aes(x=Language, y=count)) + 
+  geom_bar(stat="identity", width=.5, fill="tomato3") + 
+  labs(title="Frequency Distribution of Language") + 
+  theme(axis.text.x = element_text(angle=0, vjust=0.6)) + 
+  geom_text(aes(label=count), hjust = -0.25, vjust= 0.5, color="black", size=3.5)+coord_flip()
+
+###### filter the users
+ratings_per_user <- ratings %>% group_by(user_id) %>% summarise(number_of_ratings = unique(length(book_id)))
+ratings_per_user <- ratings_per_user[ratings_per_user$number_of_ratings >= 80,]
+
+ratings1 <- ratings[ratings$user_id %in% ratings_per_user$user_id,] 
+ratings_matrix <- dcast(data = ratings1, user_id~book_id, value.var = "rating")
+
+total_ratings_matrix <- dcast(data = ratings, user_id~book_id, value.var = "rating")
+
