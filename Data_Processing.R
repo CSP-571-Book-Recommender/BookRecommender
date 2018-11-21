4bfc3e15845aaf24e10bb3dbdc85691b7bc154b1
eshna sengupta <eshna1995@hotmail.com>
2018-11-21 17:38:21 -0600

    code cleanup and segregation

--- Data_Processing.R
+++ Data_Processing.R
@@ -7,6 +7,7 @@ library(recommenderlab)
 updated_ratings_matrix <- dcast(data = eng_book_ratings, user_id~book_id, value.var = "rating")
 # write.csv(updated_ratings_matrix, file = "updated_ratings_matrix.csv")
 
+#Converting it into a sparse matrix to save space 
 updated_ratings_matrix[is.na(updated_ratings_matrix)]<- 0
 updated_ratings_matrix = as.matrix(updated_ratings_matrix[,-1])
 sparse_ratings <- as(updated_ratings_matrix, "sparseMatrix")
@@ -16,27 +17,10 @@ gc()
 ####### creating the ratings matrices
 # memory.limit(size = 180000)
 real_ratings <- new("realRatingMatrix", data = sparse_ratings)
-real_ratings
 
-
-####### normalizing the ratings matrices
+####### normalizing the ratings matrix
 normalized_ratings <- normalize(real_ratings)
-save(normalized_ratings, file = "normalized_ratings.RData")
-rec_mod = Recommender(normalized_ratings, method = "UBCF", param=list(method="Cosine",nn=10))
-Top_5_pred = predict(rec_mod, normalized_ratings[1,], n=10)
-Top_5_List = as(Top_5_pred, "list")
-Top_5_List
-
-#Convert to data frame, change column name to book id 
-Top5_df <- as.data.frame(Top_5_List)
-names(Top5_df) <- "book_id"
-head(Top5_df)
-Top5_df$book_id <- as.numeric(Top5_df$book_id)
-class(Top5_df$book_id)
-recommended_books = left_join(Top5_df, all_books, by = "book_id")
-recommended_books$title
-class(all_books$book_id)
-# save(norm_updated_ratings_mat, file = "norm_updated_ratings_mat.RData")
+#save(normalized_ratings, file = "normalized_ratings.RData")
 
 ####### binarising the ratings matrices
 #bin_ratings_mat <- binarize(updated_ratings_matrix, minRating = 3)
